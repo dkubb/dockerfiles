757467767f9154ff0e1c581a9b4718cad2069fb0
Dan Kubb <dan.kubb@gmail.com>
2015-09-06 00:20:56 -0700

    Change default database to be postgres
    * Change rails configuration to use postgres instead of sqlite

--- alpine-nix-rails-nginx/example-rails-template.rb
+++ alpine-nix-rails-nginx/example-rails-template.rb
@@ -26,16 +26,42 @@ file 'Procfile', <<-'PROCFILE'
 web: bundle exec puma --quiet --config config/puma.rb
 PROCFILE
 
+file 'config/postgres.sh', <<-'POSTGRES'
+#!/usr/bin/env bash
+
+set -o errexit -o pipefail -o noglob -o noclobber -o nounset
+IFS=$'\n\t'
+
+export PATH="/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:$PATH"
+
+exec chpst -u postgres -- postgres -D $PGDATA
+POSTGRES
+
 file 'Dockerfile', <<-'DOCKERFILE'
 FROM dkubb/alpine-nix-rails-nginx:latest
 MAINTAINER Dan Kubb <dkubb@fastmail.com>
 
 ENV RAILS_ENV development
+ENV PGDATA    /var/lib/postgresql/data
 
-RUN nix-env --install sqlite-3.8.11.1
-
+COPY config/postgres.sh /etc/sv/postgres
 RUN source /etc/profile.d/nix.sh \
-  && bundle config --local build.sqlite3 '--use-system-libraries --with-zlib-dir=/nix/var/nix/profiles/default --with-sqlite3-dir=/nix/var/nix/profiles/default'
+  && /root/setup-directories.sh root /etc/sv /etc/service/postgres \
+  && /root/setup-directories.sh postgres "$(dirname "$PGDATA")" "$PGDATA" \
+  && chmod u+x /etc/sv/postgres \
+  && ln -s -- /etc/sv/postgres /etc/service/postgres/run
+
+# Setup default user and database
+USER postgres
+RUN source /etc/profile.d/nix.sh \
+  && pg_ctl initdb --pgdata $PGDATA \
+  && pg_ctl start --pgdata $PGDATA \
+  && until psql --command 'SELECT 1' 2>/dev/null >&2; do :; done \
+  && createuser rails \
+  && createdb --owner rails example_development \
+  && pg_ctl stop --pgdata $PGDATA
+
+USER root
 
 # TODO: use the bundix tool to package up Gemfile deps, see:
 # https://nixos.org/nixpkgs/manual/#sec-language-ruby

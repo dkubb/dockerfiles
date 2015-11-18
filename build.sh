a69f487708a3a22a3f0425e1c11de8bb32b90320
Markus Schirp <mbj@schirp-dso.com>
2015-11-18 22:08:02 +0000

    Add buildkite target

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ IFS=$'\n\t'
 
 readonly IMAGES=(
   alpine-rails-nginx
+  alpine-rails-buildkite
   alpine-rails-nginx/example
 )
 
-(cd alpine-rails-nginx && rails new example --template example-rails-template.rb --database postgresql --force)
+(cd alpine-rails-nginx && gem install rails && rails new example --template example-rails-template.rb --database postgresql --force)
 
 for image in "${IMAGES[@]}"; do
   (cd "$(dirname "$0")/$image" && docker build --tag "dkubb/$image" .)

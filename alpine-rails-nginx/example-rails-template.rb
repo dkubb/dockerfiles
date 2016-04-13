7fd5adfb08a09ec58711a3368f75072959f542c4
Dan Kubb <dkubb@fastmail.com>
2016-04-13 15:22:43 -0700

    Upgrade system dependencies

--- alpine-rails-nginx/example-rails-template.rb
+++ alpine-rails-nginx/example-rails-template.rb
@@ -42,7 +42,7 @@ MAINTAINER Dan Kubb <dkubb@fastmail.com>
 ENV RAILS_ENV=development \
   PGDATA=/var/db/postgresql/data
 
-RUN apk add postgresql-dev=9.5.1-r0 \
+RUN apk add postgresql-dev=9.5.2-r1 \
   && chown postgres: /usr/bin/postgres \
   && chmod 0700 /usr/bin/postgres
 
